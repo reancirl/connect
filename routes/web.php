403ebcd5a21947a4646e67fe0b30fce5734c059c
reancirl <reancirl@gmail.com>
2022-01-11 23:46:24 +0800

    [fix] middleware thankyou page

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,9 @@ Route::get('/', function () {
 
 Auth::routes(['register' => false]);
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-
 Route::get('/thank-you', [HomeController::class, 'thankyou'])->name('thankyou');
+
+Route::get('/home', [HomeController::class, 'index'])->name('home')->middleware('auth');
 
 Route::middleware(['auth'])->group(function () {
     Route::resource('post', PostController::class);
