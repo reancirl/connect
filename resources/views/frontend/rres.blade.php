9c797a3d6ed885335fb618314fb8a40afa6785eb
reancirl <reancirl@gmail.com>
2022-01-13 00:17:06 +0800

    [update] thank you page and rree page

--- resources/views/frontend/rres.blade.php
+++ resources/views/frontend/rres.blade.php
@@ -1,15 +1,27 @@
 @extends('layouts.frontend')
 
 @section('content')
-     <div class="container-fluid" style="height: 300px;">
+    <div class="container-fluid" style="height: 300px;">
         <div class="row justify-content-center">
-
-            <div class="col-md-5">
-                <h1 style="color: #403254;font-weight: bold;font-size: 55px;">Buying and Selling is at Our Core</h1>
-                <p >Despite our end-to-end suite of <u style="color: #1e73be">investment services</u>, Connect.ca hasn’t lost touch with the core functions every real estate brokerage is built on – buying and selling property.</p>
+            <div class="col-md-5" style="margin-top:4%;">
+                <h1 style="color: #403254;font-weight: 800;font-size: 4rem;">
+                    Buying and Selling
+                    <br> 
+                    is at Our Core
+                </h1>
+                <p style="color:#000; font-size:1.3rem;" class="lead">Despite our end-to-end suite of 
+                    <u style="color: #1e73be">
+                        investment services
+                    </u>
+                    , Connect.ca hasn’t lost 
+                    <br> 
+                    touch with the core functions every real estate brokerage is built on – buying 
+                    <br>                    
+                    and selling property.</p>
             </div>
-            <div class="">
-                <img alt="image" src="img/rres.png" id="rres_png">
+
+            <div>
+                <img alt="image" src="{{ asset('img/rres.png') }}" id="rres_png">
                 <style type="text/css">
                     @media (max-width:629px) {
                         img#rres_png {
@@ -22,37 +34,49 @@
         </div>
     </div>
     <br><br><br><br>
-    <div style="background-color: #f7f8f9;">
+    <div style="background-color: #f1f2f9;">
     <br>
-    <div class="container">
+    <div class="container mb-4">
         <div align="center">
            <div class="text-block"></div>
             <div class="col-lg-7">
                 <div class="text-block">
-                    <h5 style="text-align: center;">With the addition of Lisa Andrews, a storied resale real estate professional with decades of experience in building and leading teams of top-class real estate brokers to numerous awards and achievements, Connect.ca also knows how to help clients buy and sell single properties better than the competition.</h5>
-                   
+                    <p style="color:#000; font-size:1.3rem;" class="lead text-center">With the addition of Lisa Andrews, a storied resale real estate professional with decades of experience in building and leading
+                         teams of top-class real estate brokers to numerous awards and achievements, Connect.ca also knows how to help clients buy and sell single properties better 
+                         than the competition.</p>
                 </div>
-                <div class="text-block">
-                   <button class="btn rounded-pill" style="background-color: #7dc9d1;width: 270px;height: 45px;color: #3a3594;border-radius: 25px;border: none;font-size: 16px;">What Your Home Worth?</button>
-                   <button class="btn rounded-pill" style="background-color: #7dc9d1;width: 270px;height: 45px;color: #3a3594;border-radius: 25px;border: none;font-size: 16px;">Connect with a Buyer's Agent</button>
-                </div>
+                <a class="btn_connect_button mr-5">What Your Home Worth?</a>
+                <a class="btn_connect_button">Connect with a Buyer's Agent</a>
             </div>
         </div>
-        
     </div>
     
     <br>
-     <div class="container">
+    <div class="container">
         <div class="row justify-content-around">
-             <div class="col-md-6"> <img alt="Image" src="img/2-Exterior.jpg" style="width: 1100px;"> </div>
+             <div class="col-md-6"> <img alt="Image" src="{{ asset('img/2-Exterior.jpg') }}" style="width: 1100px;"> </div>
             <div class="col-md-6">
                 <div class="switchable__text">
 
                     <div class="text-block"></div><div class="text-block"></div>
                     <h2 style="color: #403254;font-size: 25px;font-weight: bold;font-family: Ropa Sans, Sans-serif;">The Resale Experience</h2>
-                    <p class="lead" style="font-size: 17px;"> While we believe that every property owner already has the potential to become a successful investor, we know that many transactions with our clientele will solely be a one-time purchase or sale. </p> <a href="#" style="color: #3A3594;float: right;text-decoration: underline;">Read More »</a> </div>
+                    <p class="lead" style="font-size: 1.2rem;color:#000;"> While we believe that every property owner already has the potential to become a successful investor, we know that many transactions with our clientele will solely be a one-time purchase or sale. </p> 
+                    <div class="float-right">
+                        <ul class="accordion accordion-2">
+                            <li class="">
+                                <div class="accordion__title" style="opacity:1;"> 
+                                    <span class="h5 float-right" style="color:#3A3594;text-decoration:underline;">Read More ></span>
+                                 </div>
+                                <div class="accordion__content">
+                                    <p class="lead" style="color:#000;font-size:1.2rem;"> Stack follows the BEM naming convention that focusses on logical code readability that is reflected in both the HTML and CSS.
+                                         Interactive elements such as accordions and tabs follow the same markup patterns making rapid development easier for developers and beginners
+                                          alike. </p>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-           
         </div>
     </div>
     <br><br><br>
@@ -62,18 +86,50 @@
                 <div class="switchable__text">
                     <div class="text-block"></div><div class="text-block"></div>
                     <h2 style="color: #403254;font-size: 25px;font-weight: bold;font-family: Ropa Sans, Sans-serif;">Home Advantage</h2>
-                    <p class="lead" style="font-size: 17px;"> Our customer service second-to-none in the industry. Along with access to our in-house real estate brokerage, Connect.ca Realty, we also utilize the services of in-house team of VIP Platinum Realtors, as well as our in-house Resale network, and our OneDayAssignments and OneDayPurchase programs – all to our clients’ full advantage. </p> <a href="#" style="color: #3A3594;float: right;text-decoration: underline;">Read More »</a> </div>
+                    <p class="lead" style="font-size: 1.2rem;color:#000;"> WOur customer service second-to-none in the industry. Along with access to our in-house real estate brokerage, Connect.ca Realty, we also utilize the services of in-house team of VIP Platinum Realtors, as well as our in-house Resale network, and our OneDayAssignments and OneDayPurchase programs – all to our clients’ full advantage. </p> 
+                    <div class="float-right">
+                        <ul class="accordion accordion-2">
+                            <li class="">
+                                <div class="accordion__title" style="opacity:1;"> 
+                                    <span class="h5 float-right" style="color:#3A3594;text-decoration:underline;">Read More ></span>
+                                 </div>
+                                <div class="accordion__content">
+                                    <p class="lead" style="color:#000;font-size:1.2rem;"> Stack follows the BEM naming convention that focusses on logical code readability that is reflected in both the HTML and CSS.
+                                         Interactive elements such as accordions and tabs follow the same markup patterns making rapid development easier for developers and beginners
+                                          alike. </p>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-            <div class="col-md-6"> <img alt="Image" src="img/3-InteriorPlaceHolder.jpg" style="width: 1100px;"> </div>
+            <div class="col-md-6"> <img alt="Image" src="{{ asset('img/3-InteriorPlaceHolder.jpg') }}" style="width: 1100px;"> </div>
         </div>
     </div>
     <br><br>
-    <section style="background-color:#3A3594;width: 100%;height: 477px;opacity: 1;">
-        <div class="container">
-            <div style="text-align: center;">
-                <h1 style="color: #FFDE59;font-size: 35px;font-weight: 600;font-family: Roboto, Sans-serif;">Sell with Us Today</h1>
-                <p class="lead" style="color: #FFDE59;font-size: 22px;font-weight: 700;font-family: Ropa Sans, Sans-serif;"> Selling with Connect.ca is the easiest, most integrated<br>selling experience on the market. To sell with the best,<br>click below. </p>
-                <button href="#" style="border-radius: 20px;width: 150px;background-color: #79c9d1;font-size: 16px;"> Get Selling </button>
+    <section class="unpad pb-0 mb-0">
+        <div class="imagebg text-center height-50" style=" background: linear-gradient(rgba(58, 53, 148, 0.8), rgba(58, 53, 148, 0.8)), url('{{ asset('uploads/CTAPanel.png') }}');; background-repeat: no-repeat; background-size: cover;">
+            <div class="background-image-holder"> </div>
+            <div class="container pos-vertical-center">
+                <div class="row" >
+                    <div class="col-md-12">
+                        <div class="article__title">
+                            <h1 style="font-family:Roboto, Sans-serif; color: #FFDE59; font-size: 50px; font-weight:800;">
+                                Sell with Us Today
+                            </h1>
+                            <p style="text-align: center; color: #FFDE59; font-family: Ropa Sans, Sans-serif; font-size: 22px; font-weight: 700;">
+                                Selling with Connect.ca is the easiest, most integrated
+                                <br>
+                                selling experience on the market. To sell with the best,
+                                <br>
+                                click below.
+                            </p>
+
+                            <a class="btn_connect_button" style="padding-left:2rem !important;padding-right:2rem !important;">Get Selling</a>
+                            
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
--- resources/views/frontend/thankyou.blade.php
+++ resources/views/frontend/thankyou.blade.php
@@ -1,108 +1,57 @@
 @extends('layouts.frontend')
 
 @section('content')
-<style>
-    /* .flex-parent 
-    {
-  display: flex;
-    }
-    button 
-    {
-        border-radius: 50px;
-        width: 10px;
+    <section class="text-center imagebg mb-5 pb-3">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-lg-8">
+                    <h1 style="color: #403254; font-size: 4rem; font-weight:900;" >
+                        Thanks for Connecting!
+                    </h1>
+                    <p class="lead" style="color:#000000">
+                        We’re excited to learn more about your real estate ambitions. We’ll be in touch as
+                        <br>
+                        soon as we’ve read over your information.
+                    </p>
+                    <p class="lead" style="color:#000000">
+                        If you have any other questions, please reach out to us at <br>
+                        <span style="color:#3a3594;text-decoration:underline;font-weight:700;">admin@connect.ca</span> or <span style="color:#3a3594;text-decoration:underline;font-weight:700;">416.800.9272</span>
+                    </p>
 
-    }
-
-.jc-center 
-{
-  justify-content: center;
-}
- 
-button.margin-right {
-  margin-right: 20%;
-}  */
-
-.my-button {
-    display: inline-block;
-    margin: 20px;
-    background:#79C9D1; 
-    color: #3A3594; 
-    fill: #3A3594;
-    font-size: 17px;
-    font-weight: 800;
-    font-family: 'Ropa Sans', Sans-serif!important;
-    border-radius: 50px;
-    padding: 8px 16px;
-    /* -moz-transition: all 0.2s;
-    -webkit-transition: all 0.2s; */
-    transition: all 0.2s;
-}
-.my-button:hover {
-    background: #666;
-    color: #c1e1e0;
-}
-
-</style>
-<div class="nav-container"> </div>
-        <div class="main-container">
-            <section class="text-center imagebg">
-                <div class="background-image-holder"> <img alt="background" src="img/hero-1.jpg"> </div>
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-10 col-lg-8">
-                            <h1 style="color: #403254; font-size: 55px; font-weight:800;" >
-                                Thanks for Connecting!
-                            </h1>
-                            <p class="lead" style="color:#000000"> We’re excited to learn more about your real estate ambitions. We’ll be in touch as
-                                soon as we’ve read over your information.
-                             </p>
-                            <p style="color:#000000"> If you have any other questions, please reach out to us at <br>
-                                <u style="color:#403254"><b>admin@connect.ca</b></u>  or <u style="color:#403254"><b>416.800.9272</b></u>
-                            </p>
-
-                        </div>
-                    </div>
                 </div>
-            </section>
-            <section class="text-center">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-12 col-lg-10">
-                            <img width="1000" height="1067" alt="background" src="{{ asset('img/thankyou-person.jpg') }}">
-                        </div>
-                    </div>
+            </div>
+        </div>
+    </section>
+    <section class="text-center pt-0 mt-0">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-lg-10">
+                    <img alt="background" src="{{ asset('img/thankyou-person.jpg') }}">
                 </div>
+            </div>
+        </div>
+    </section>
 
-                <br>
-                <br>
-                <br>
-                <br>
+    <section class="unpad pb-0 mb-0">
+    
+            <div class="imagebg text-center height-50" style=" background: linear-gradient(rgba(58, 53, 148, 0.8), rgba(58, 53, 148, 0.8)), url('{{ asset('uploads/CTAPanel.png') }}');; background-repeat: no-repeat; background-size: cover;">
+                <div class="background-image-holder"> </div>
+                <div class="container pos-vertical-center">
+                    <div class="row" >
+                        <div class="col-md-12">
+                            <div class="article__title">
+                                <h1 style="font-family:Robot, Sans-serif; color: #FFDE59; font-size: 50px; font-weight:800;">We’re Your Full-Service <br> Real Estate Brokerage</h1>
+                                <p style="text-align: center; color: #FFDE59; font-family: Ropa Sans, Sans-serif; font-size: 22px; font-weight: 700;">
+                                    Whether you you’re looking to buy, sell, manage, or invest, we cover the entire <br> real estate journey to meet all of your needs.
+                                </p>
 
-            <section class="unpad">
-            
-                    <div class="imagebg text-center height-70" style=" background: linear-gradient(rgba(58, 53, 148, 0.8), rgba(58, 53, 148, 0.8)), url('{{ asset('uploads/CTAPanel.png') }}');; background-repeat: no-repeat; background-size: cover;">
-                        <div class="background-image-holder"> </div>
-                        <div class="container pos-vertical-center">
-                            <div class="row" >
-                                <div class="col-md-12">
-                                    <div class="article__title">
-                                        <h1 style="font-family:Robot, Sans-serif; color: #FFDE59; font-size: 50px; font-weight:800;">We’re Your Full-Service <br> Real Estate Brokerage</h1>
-                                        <p style="text-align: center; color: #FFDE59; font-family: Ropa Sans, Sans-serif; font-size: 22px; font-weight: 700;">
-                                            Whether you you’re looking to buy, sell, manage, or invest, we cover the entire <br> real estate journey to meet all of your needs.
-                                        </p>
-
-                                        <button class="my-button" title="Relevant Title" href="#">Selling & Buying</button>
-                                        <button class="my-button" title="Relevant Title" href="#">Investment Service</button>
-                                      
-                                    </div>
-                                </div>
+                                <a class="btn_connect_button">Selling & Buying</a>
+                                <a class="btn_connect_button">Investment Service</a>
+                                
                             </div>
                         </div>
                     </div>
-            </section>
-        </div> 
-
-    
-
-
+                </div>
+            </div>
+    </section>
 @endsection
